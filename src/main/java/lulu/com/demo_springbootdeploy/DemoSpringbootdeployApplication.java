74030cb785ade4da574233d2e9438f9ac6ed4c2b
Lulu <a0953687095@gmail.com>
2024-08-28 01:19:46 +0800

    Initial commit

---
+++ src/main/java/lulu/com/demo_springbootdeploy/DemoSpringbootdeployApplication.java
@@ -0,0 +1,13 @@
+package lulu.com.demo_springbootdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootdeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootdeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lulu/com/demo_springbootdeploy/DemoSpringbootdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package lulu.com.demo_springbootdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootdeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
